ba7b889f972504e55fb0b2492e486631fa4fe917
vi66ko <vi66ko@mail.bg>
2025-04-20 12:34:49 +0100

    feat(Debug): adding getter for the debuging state. In this way we can snesitive information olnly in debugging state

--- Debug.java
+++ Debug.java
@@ -2,34 +2,35 @@
  * Print information about the running program
  */
 
-public class Debug
-{
+public class Debug {
   private static boolean debug = true;
 
   /**
    * Set true/false to print debugging information
+   * 
    * @param state Debugging true false
    * @return The old state
    */
-  public static synchronized boolean set( boolean state )
-  {
+  public static synchronized boolean set(boolean state) {
     boolean oldState = debug;
     debug = state;
     return oldState;
   }
 
+  public static synchronized boolean getDebugState() {
+    return debug;
+  }
+
   /**
    * Display text for debugging purposes
-   * @param fmt  The same as printf etc
+   * 
+   * @param fmt    The same as printf etc
    * @param params The parameters to fmt
    */
-  public static void trace(String fmt, Object... params )
-  {
-    if ( debug )
-    {
-      synchronized( Debug.class )
-      {
-        System.out.printf( fmt, params );
+  public static void trace(String fmt, Object... params) {
+    if (debug) {
+      synchronized (Debug.class) {
+        System.out.printf(fmt, params);
         System.out.println();
       }
     }
@@ -37,27 +38,26 @@ public class Debug
 
   /**
    * Display a fatal message if the assertion fails
-   * @param ok true if all is ok
-   * @param fmt The same as printf etc
+   * 
+   * @param ok     true if all is ok
+   * @param fmt    The same as printf etc
    * @param params The parameters to fmt
    */
-  public static void assertTrue( boolean ok, String fmt, 
-                                 Object... params )
-  {
-    if ( ! ok )
-    {
-      error( "Assert - " + fmt, params );
+  public static void assertTrue(boolean ok, String fmt,
+      Object... params) {
+    if (!ok) {
+      error("Assert - " + fmt, params);
     }
   }
 
   /**
    * Display a fatal message
-   * @param fmt The same as printf etc
+   * 
+   * @param fmt    The same as printf etc
    * @param params The parameters to fmt
    */
-  public static synchronized void error(String fmt, Object... params )
-  {
-    System.out.printf( "ERROR: " + fmt, params );
+  public static synchronized void error(String fmt, Object... params) {
+    System.out.printf("ERROR: " + fmt, params);
     System.out.println();
   }
 
